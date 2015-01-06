3518c0d182548d74aac83865458617d632f1ac4f
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-01-07 00:10:07 +1300

    Added clenup generator for tags

--- script.py
+++ script.py
@@ -4,7 +4,10 @@ import requests
 page = requests.get('http://store.steampowered.com/app/211420/')
 tree = html.fromstring(page.text)
 
-name = tree.xpath('//div[@class="apphub_AppName"]/text()')
-price = tree.xpath('//meta[@itemprop="price"]/@content')
-tags = tree.xpath("//div[@class=\"glance_tags popular_tags\"]/a/text()")
+name = tree.xpath('//div[@class="apphub_AppName"]/text()')[0]
+price = tree.xpath('//meta[@itemprop="price"]/@content')[0]
+tags = [
+		" ".join(item.split()) for item 
+		in tree.xpath("//div[@class=\"glance_tags popular_tags\"]/a/text()")
+	]
 genre = tree.xpath("//b[text()='Genre:']/following-sibling::a[count(following-sibling::b)=3]/text()")
