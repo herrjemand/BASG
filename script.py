e937d83cfd890fc7dac0f6005e1b1b2bb3774251
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-01-07 00:16:05 +1300

    Optimised search for genre/publisher/developer

--- script.py
+++ script.py
@@ -5,9 +5,20 @@ page = requests.get('http://store.steampowered.com/app/211420/')
 tree = html.fromstring(page.text)
 
 name = tree.xpath('//div[@class="apphub_AppName"]/text()')[0]
+
 price = tree.xpath('//meta[@itemprop="price"]/@content')[0]
+
 tags = [
 		" ".join(item.split()) for item 
 		in tree.xpath("//div[@class=\"glance_tags popular_tags\"]/a/text()")
 	]
-genre = tree.xpath("//b[text()='Genre:']/following-sibling::a[count(following-sibling::b)=3]/text()")
+
+metacache = tree.xpath("//b[text()='Genre:']/following-sibling::a/text()")
+
+genre = metacache[:2]
+
+publisher = metacache[2:][1]
+
+developer = metacache[2:][0]
+
+# appid = 
