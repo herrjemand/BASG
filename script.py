a2030b4d3b56173051912b1fe1af2a8baff8d7c6
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-01-07 00:28:25 +1300

    Added appid and release date. Improved style

--- script.py
+++ script.py
@@ -4,16 +4,16 @@ import requests
 page = requests.get('http://store.steampowered.com/app/211420/')
 tree = html.fromstring(page.text)
 
-name = tree.xpath('//div[@class="apphub_AppName"]/text()')[0]
+name = tree.xpath('//div[@class=\'apphub_AppName\']/text()')[0]
 
-price = tree.xpath('//meta[@itemprop="price"]/@content')[0]
+price = tree.xpath('//meta[@itemprop=\'price\']/@content')[0]
 
 tags = [
-		" ".join(item.split()) for item 
-		in tree.xpath("//div[@class=\"glance_tags popular_tags\"]/a/text()")
+		' '.join(item.split()) for item 
+		in tree.xpath('//div[@class=\'glance_tags popular_tags\']/a/text()')
 	]
 
-metacache = tree.xpath("//b[text()='Genre:']/following-sibling::a/text()")
+metacache = tree.xpath('//b[text()=\'Genre:\']/following-sibling::a/text()')
 
 genre = metacache[:2]
 
@@ -21,4 +21,6 @@ publisher = metacache[2:][1]
 
 developer = metacache[2:][0]
 
-# appid = 
+appid = tree.xpath('//div[@class=\'glance_tags popular_tags\']/@data-appid')[0]
+
+release_date = tree.xpath('//div[@class=\'release_date\']/span[@class=\'date\']/text()')
